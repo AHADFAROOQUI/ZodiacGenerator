545996551a54c2e84e57c3bfb15ec37c362de90e
AHADFAROOQUI <sykersson@gmail.com>
2019-07-13 14:13:49 -0400

    Added first case statement. I'm now testing it before I apply it to all conditions.

--- ZodiacGenerator.sh
+++ ZodiacGenerator.sh
@@ -30,7 +30,7 @@ year=`echo $bday | cut -d ' ' -f 3`
 
 
 january=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31)
-febuary=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28)
+february=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28)
 march=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31)
 april=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30)
 may=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31)
@@ -42,21 +42,26 @@ october=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27
 november=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30)
 december=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31)
 
-aries=("`echo ${mar[@]} | cut -d ' ' -f 21-31`" "`echo ${apr[@]} | cut -d ' ' -f 1-19`")
-taurus=("`echo ${apr[@]} | cut -d ' ' -f 20-30`" "`echo ${may[@]} | cut -d ' ' -f 1-20`")
-gemini=("`echo ${may[@]} | cut -d ' ' -f 21-31`" "`echo ${jun[@]} | cut -d ' ' -f 1-20`")
-cancer=("`echo ${jun[@]} | cut -d ' ' -f 21-30`" "`echo ${jul[@]} | cut -d ' ' -f 1-22`")
-leo=("`echo ${jul[@]} | cut -d ' ' -f 23-31`" "`echo ${aug[@]} | cut -d ' ' -f 1-22`")
-virgo=("`echo ${aug[@]} | cut -d ' ' -f 23-31`" "`echo ${sep[@]} | cut -d ' ' -f 1-22`")
-libra=("`echo ${sep[@]} | cut -d ' ' -f 23-30`" "`echo ${oct[@]} | cut -d ' ' -f 1-22`")
-scorpio=("`echo ${oct[@]} | cut -d ' ' -f 23-31`" "`echo ${nov[@]} | cut -d ' ' -f 1-21`")
-sagittarius=("`echo ${nov[@]} | cut -d ' ' -f 22-30`" "`echo ${dec[@]} | cut -d ' ' -f 1-21`")
-capricorn=("`echo ${dec[@]} | cut -d ' ' -f 22-31`" "`echo ${jan[@]} | cut -d ' ' -f 1-19`")
-aquarius=("`echo ${jan[@]} | cut -d ' ' -f 20-31`" "`echo ${feb[@]} | cut -d ' ' -f 1-18`")
-pisces=("`echo ${feb[@]} | cut -d ' ' -f 19-28`" "`echo ${mar[@]} | cut -d ' ' -f 1-20`")
+aries=("`echo ${march[@]} | cut -d ' ' -f 21-31`" "`echo ${april[@]} | cut -d ' ' -f 1-19`")
+taurus=("`echo ${april[@]} | cut -d ' ' -f 20-30`" "`echo ${may[@]} | cut -d ' ' -f 1-20`")
+gemini=("`echo ${may[@]} | cut -d ' ' -f 21-31`" "`echo ${june[@]} | cut -d ' ' -f 1-20`")
+cancer=("`echo ${june[@]} | cut -d ' ' -f 21-30`" "`echo ${july[@]} | cut -d ' ' -f 1-22`")
+leo=("`echo ${july[@]} | cut -d ' ' -f 23-31`" "`echo ${august[@]} | cut -d ' ' -f 1-22`")
+virgo=("`echo ${august[@]} | cut -d ' ' -f 23-31`" "`echo ${september[@]} | cut -d ' ' -f 1-22`")
+libra=("`echo ${september[@]} | cut -d ' ' -f 23-30`" "`echo ${october[@]} | cut -d ' ' -f 1-22`")
+scorpio=("`echo ${october[@]} | cut -d ' ' -f 23-31`" "`echo ${november[@]} | cut -d ' ' -f 1-21`")
+sagittarius=("`echo ${november[@]} | cut -d ' ' -f 22-30`" "`echo ${december[@]} | cut -d ' ' -f 1-21`")
+capricorn=("`echo ${december[@]} | cut -d ' ' -f 22-31`" "`echo ${january[@]} | cut -d ' ' -f 1-19`")
+aquarius=("`echo ${january[@]} | cut -d ' ' -f 20-31`" "`echo ${february[@]} | cut -d ' ' -f 1-18`")
+pisces=("`echo ${february[@]} | cut -d ' ' -f 19-28`" "`echo ${march[@]} | cut -d ' ' -f 1-20`")
 
 case $month in
     "1")
-        echo january
+	if echo "${aquarius[0]}" | grep -q "$day"
+	then
+	    echo january
+	else
+	    echo "${aquarius[0]}"
+	fi
         ;;
 esac
